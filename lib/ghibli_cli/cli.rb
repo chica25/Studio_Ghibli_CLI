584e967a4b3515c68f7ddc6aefc4437c09dfa94a
chica25 <patricia@patriciaco.com>
2020-06-07 10:08:00 -0400

    edit gets.chomp

--- lib/ghibli_cli/cli.rb
+++ lib/ghibli_cli/cli.rb
@@ -8,7 +8,7 @@ class Cli
 
     #1 welcome the user
     def run
-        puts "Welcome to Ghibli films!"
+        puts "WELCOME TO STUDIO GHIBLI FILMS!"
         sleep(1)
         Api.get_movies
         movie_list
@@ -17,9 +17,9 @@ class Cli
         selection
     end
 
-        def user_input
-            gets.chomp 
-        end
+    def user_input
+        gets.chomp.downcase
+    end
 
     def movie_title
         Studio_Ghibli.all.each_with_index do |film, index|
@@ -32,49 +32,30 @@ class Cli
         movie_title
     end
 
-    #if user selects a movie title
-    def test
-        Studio_Ghibli.all.find do |film| 
-             film.title = "Ponyo"
-            puts film.title
-        end
-    end
-
-    def selection
-        puts "Would you like to pick another movie? y/n"
-        input = user_input
+    def movie_list
+           puts "Would you like to see the list of movies playing this week? y/n"
+           input = user_input
         if input == "yes" || input == "y"
-            print_movie_titles
-            list_selection
-            selection
+           print_movie_titles
         else
            puts "Goodbye"
         end
     end
 
     def pick_movie
-        puts "Please select a number"
+        puts "Please select a number:"
         input = gets.chomp.to_i 
     end
 
-    def movie_list
-        puts "Do you want to see the list of movies playing this week, Y/N?"
-        input = user_input
-        if input == "yes" || input == "y"
-          print_movie_titles
-        else
-            puts "Goodbye"
-        end
-    end
 
     def movie_selection
-        puts "Please select a number of the movie"
+        puts "Please select a movie number:"
     end
 
     def list_selection
-        index = gets.chomp.to_i - 1
-        last_element = Studio_Ghibli.all.size - 1
-        # if index.between?(0..-1) 
+            index = gets.chomp.to_i - 1
+            last_element = Studio_Ghibli.all.size - 1
+            # if index.between?(0..-1) 
         if index.between?(0,last_element) 
             movie = Studio_Ghibli.all[index]
             puts "Movie Title:" + " " << movie.title 
@@ -82,14 +63,26 @@ class Cli
             puts "Director:" + " " << movie.director
             puts "Description:" + " " << movie.description
         else
-            puts "Please enter a valid number"
+            puts "Please enter a valid number:"
             list_selection
         end
     end
     
-
-    # def goodbye
-    #     puts "Goodbye!"
-    # end
+    def selection
+            puts "Would you like to pick another movie? y/n"
+            input = user_input
+        if input == "yes" || input == "y"
+            print_movie_titles
+            list_selection
+            selection
+        #elsif input == "n" || input == "no"
+        else 
+           puts "Goodbye"
+        end
+    end
 
 end
+
+#notes - need to delete the n after the user enters "n"
+# need to fix the upcase if the user enters a capital Y or N
+# selection method - elsif 
