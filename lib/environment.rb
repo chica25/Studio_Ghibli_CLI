48fc3e6f9fed69b225de71445809f145066d79db
chica25 <patricia@patriciaco.com>
2020-06-17 19:20:05 -0400

    updated file names

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,9 +1,9 @@
 require 'bundler/setup'
 Bundler.require(:default)
 
-require_relative './ghibli_cli/cli.rb'
-require_relative './ghibli_cli/api.rb'
-require_relative './ghibli_cli/studio_ghibli.rb'
+require_relative './ghibli_cli/cli'
+require_relative './ghibli_cli/api'
+require_relative './ghibli_cli/studio_ghibli'
 
 
  
